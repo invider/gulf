5de362abea8cfd37ad662e53f93ca14c4966d263
Igor Khotin <khotin@gmx.com>
2020-08-10 22:25:27 +0300

    [+] collider

---
+++ dna/collider.js
@@ -0,0 +1,19 @@
+function evo(dt) {
+    const ls = this.__.tie._ls
+    const tls = this.__.bio._ls
+
+    const I = ls.length
+    const J = tls.length
+    for (let i = 0; i < I; i++) {
+        const e = ls[i]
+        if (e.head && !e.dead) {
+            const head = e.head
+            for (let j = 0; j < J; j++) {
+                const target = tls[j]
+                if (target.touch && target !== head) {
+                    target.touch(head)
+                }
+            }
+        }
+    }
+}
